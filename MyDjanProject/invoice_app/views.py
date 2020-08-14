aa45840aaae9014f302a59fa16b29d3762c1c7e0
vikaskushwah89 <vikaskushwah89@gmail.com>
2020-08-14 12:10:36 +0530

    Handling Views

--- MyDjanProject/invoice_app/views.py
+++ MyDjanProject/invoice_app/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import render
 from datetime import datetime
 
 # Create your views here.
+
+
 def home_page(request:HttpRequest):
-    return render(request, "index.html")
+    name = request.GET.get("name")
+    context = {"name": name, "time": str(datetime.now())}
+    return render(request, "index.html", context = context)
+
 
 def ping(request:HttpRequest):
     #return HttpResponse("Hello world!")
+
     now = datetime.now()
     return JsonResponse({"message": "hello world"
                             , "current_time": str(now)})
